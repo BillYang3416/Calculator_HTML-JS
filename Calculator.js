9df35eff8c61ac9f7cb9fd05b3e6a0f6f02a5af9
billyang <billyang3416@gmail.com>
2020-03-04 10:20:30 +0800

    third-commit

--- Calculator.js
+++ Calculator.js
@@ -21,6 +21,106 @@ const calculator = {
 	operator: null
 };
 
+// when user click the button, the value will be showed in screen
+function inputDigit(digit) {
+	const { displayValue, waitingForSecondOperand } = calculator; 
+
+	// the flag is change to true by click operator
+	if( waitingForSecondOperand === true) {
+		calculator.displayValue = digit;
+		calculator.waitingForSecondOperand = false;
+	} else {	
+		// if displayValue === 0, so 0 change to digit
+		// else displayValue will append the digit to it
+		calculator.displayValue = displayValue === '0' ? digit : displayValue + digit; 
+	}
+
+	console.log(calculator);
+}
+
+
+function inputDecimal(dot) {
+
+	// prevent someone press operator and then press '.'
+	//if (calculator.waitingForSecondOperand === true) return;
+
+	// if the displayValue does not contain decimal point
+	if (!calculator.displayValue.includes(dot)) {
+		// append the decimal point
+		calculator.displayValue += dot;
+	}
+} 
+
+
+
+function handleOperator(nextOperator) {
+
+	// step 1: 
+	// when an operator key is pressed,
+	// we convert the current number to float
+	// and then store the value to 'firstOperand'
+	// if it does not exist.
+	// Also set 'waitingForSecondOperand' to true
+	// means the second operand is ready to begin.
+
+	// step 2:
+	// when user finishes the second operand
+	// and hit an operator
+	// so we need to display the result of the calculation
+	//  object destructuring means more easily to extract the properties	
+	
+	const {firstOperand, displayValue , operator} = calculator;
+	const inputValue = parseFloat(displayValue);
+
+	// if user want to '7+2' but after press '+'
+	// user changes his mind, he want to '7-2'
+	// so we prevent plus action and
+	// change operator:'+' to operator:'-'
+	if(operator && calculator.waitingForSecondOperand) {
+		calculator.operator = nextOperator;
+		console.log(calculator);
+		return;
+	}
+
+
+	if (firstOperand === null) {
+		calculator.firstOperand = inputValue;
+	} else if (operator) {  // non-zero is equal to true
+		// create an object 'performCalculation'
+		// the properties is +-*/=
+		// objectName["propertyName"] access object properties
+		const result = performCalculation[operator](firstOperand,inputValue);
+		calculator.displayValue = String(result);
+		calculator.firstOperand = result;
+	}
+
+	calculator.waitingForSecondOperand = true;
+	calculator.operator = nextOperator;
+	console.log(calculator);
+}
+
+const performCalculation = {
+	'/':(firstOperand,secondOperand) => firstOperand / secondOperand,
+	
+	'*':(firstOperand,secondOperand) => firstOperand * secondOperand,
+	
+	'+':(firstOperand,secondOperand) => firstOperand + secondOperand,
+	
+	'-':(firstOperand,secondOperand) => firstOperand - secondOperand,
+	
+	'=':(firstOperand,secondOperand) => secondOperand
+
+};
+
+// AC button do reset the value
+function resetCalculator() {
+	calculator.displayValue ='0';
+	calculator.firstOperand = null;
+	calculator.waitingForSecondOperand = false;
+	calculator.operator = null;
+	console.log(calculator);
+}
+
 //updating the display
 function updateDisplay() {
 
@@ -30,31 +130,6 @@ function updateDisplay() {
 }
 updateDisplay();
 
-<<<<<<< HEAD
-// listen for clicks on the calculator and 
-// determine what type of key was clicked
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
-	const target = event.target;
-	if(!target.matches('button')) {
-		return;
-	}
-	if(target.classList.contains('operator')) {
-		console.log('operator',target.value);
-		return;
-	}
-	if(target.classList.contains('decimal')) {
-		console.log('decimal',target.value);
-		return;
-	}
-	if(target.classList.contains('all-clear')) {
-		console.log('clear',target.value);
-		return;
-	}
-	console.log('clear',target.value);
-});
-
-=======
 
 
 // listen for clicks on the calculator nad determine what type of key was clicked.
@@ -73,6 +148,8 @@ keys.addEventListener('click', (event) => {
 
 	if(target.classList.contains('operator')) {
 		console.log('operator', target.value);
+		handleOperator(target.value);
+		updateDisplay();
 		return;
 	}
 	if(target.classList.contains('decimal')) {
@@ -83,6 +160,8 @@ keys.addEventListener('click', (event) => {
 	}
 	if(target.classList.contains('all-clear')) {
 		console.log('clear', target.value);
+		resetCalculator();
+		updateDisplay();
 		return;
 	}
 
@@ -95,28 +174,3 @@ keys.addEventListener('click', (event) => {
 
 });
 
-
-// when user click the button, the value will be showed in screen
-function inputDigit(digit) {
-	const displayValue = calculator.displayValue;
-
-	// if displayValue === 0, so 0 change to digit
-	// else displayValue will append the digit to it
-	calculator.displayValue = displayValue === '0' ? digit : displayValue + digit; 
-
-}
-
-
-function inputDecimal(dot) {
-
-	// if the displayValue does not contain decimal point
-	if (!calculator.displayValue.includes(dot)) {
-		// append the decimal point
-		calculator.displayValue += dot;
-	}
-} 
-
-function handleOperator(nextOperator) {
-	
-}
->>>>>>> 77a56d8ebe66c145ebcb9b0855ba5c110b0053db
